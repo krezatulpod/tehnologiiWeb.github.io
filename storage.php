bdb2ba6c0ef35061c7337421de994e611e19f923
krezatulpod <70695542+krezatulpod@users.noreply.github.com>
2023-04-14 17:23:00 +0300

    Add files via upload

---
+++ storage.php
@@ -0,0 +1,26 @@
+<?php
+
+  $firstName = $_POST['firstName'];
+  $lastName = $_POST['lastName'];
+  $phoneNumber = $_POST['phoneNumber'];
+  $userMail = $_POST['userMail'];
+  $userMessage = $_POST['userMessage'];
+
+/* Connect to Database */
+
+$connection = new mysqli('localhost','root','','test');
+
+if($connection->connect_error ){
+    die('Connection Failed : '.$connection->connect_error);
+}else{
+    $statement = $connection->prepare("insert in contactForm(firstName,lastName,phoneNumber,userMail,userMessage)
+    values(?,?,?,?,?)");
+    $statement->bind_param("ssiss",$firstName,$lastName,$phoneNumber,$userMail,$userMessage);
+    $statement->execute();
+
+    echo "Form submit ..."
+    $statement.close();
+    $connection.close();
+}
+
+?>
